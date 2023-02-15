0c165731ef48363833978116da1f11180b7e81af
kiwi28 <alexkiwi.2011@gmail.com>
2023-02-16 01:31:16 +0200

    first build

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,84 @@
-import { StyleSheet } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Image, Platform, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({
+	navigation,
+}: RootTabScreenProps<"TabOne">) {
+	const [imgUrl, setImgUrl] = useState<string>("");
+
+	useEffect(() => {
+		axios("https://dog.ceo/api/breeds/image/random")
+			.then((res) => {
+				console.log(res.data.message);
+				setImgUrl(res.data.message);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setImgUrl;
+				console.log("finally");
+			});
+	}, []);
+
+	const handleGetNewDoggo = () => {
+		console.log(Platform);
+		axios("https://dog.ceo/api/breeds/image/random")
+			.then((res) => {
+				console.log(res.data.message);
+				setImgUrl(res.data.message);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab One</Text>
+			<Text>kiwi 123</Text>
+			<Text>kiwi 123</Text>
+			<Text>kiwi 123</Text>
+			<Text>kiwi 123</Text>
+			<Text>kiwi 123</Text>
+			<Text>kiwi 123</Text>
+			<Image
+				style={styles.image}
+				source={
+					imgUrl ? { uri: imgUrl } : require("../assets/images/dog_shadow.jpg")
+				}
+			/>
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<Button title="Get new Doggo!" onPress={handleGetNewDoggo} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+	image: {
+		width: 300,
+		height: 300,
+	},
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,53 @@
-import { StyleSheet } from 'react-native';
+import { Button, Image, Platform, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+// TODO: needs typing
+export default function TabTwoScreen({ navigation }: any) {
+	const chance = Math.random();
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Platform: {Platform.OS}</Text>
+			<Button title="Go BACK" onPress={() => navigation.goBack()} />
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<Text>Sometimes u get kiwi, sometimes u get cat</Text>
+			<Text>this time u got: {chance < 0.5 ? "cat" : "kiwi"}</Text>
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+			{chance < 0.5 ? (
+				<Image
+					style={styles.imageCat}
+					source={{ uri: "https://cataas.com/cat" }}
+				/>
+			) : (
+				<Image
+					style={styles.imageCat}
+					source={require("../assets/images/kiwi.jpg")}
+				/>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+	imageCat: {
+		width: 300,
+		height: 300,
+	},
 });
